18dd114ed65dcb2e9293040b5a6e5584f61c2416
md-sazzadul <md.sazzadul.islam15@gmail.com>
2024-12-02 17:45:19 +0600

    added smooth scrolling animation on page load

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-out",
+      },
+    },
   },
   plugins: [],
 };
